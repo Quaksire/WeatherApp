31ba51fe928330a8fd01b4ace88200c638c42467
quaksire.group <quaksire.group@gmail.com>
2019-07-24 23:39:13 +0100

    Set Drawer navigation correctly

--- app/src/main/java/com/quaksire/weatherapp/MainActivity.kt
+++ app/src/main/java/com/quaksire/weatherapp/MainActivity.kt
@@ -2,17 +2,56 @@ package com.quaksire.weatherapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.*
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListener {
 
-    lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(findViewById(R.id.nav_host_fragment))
+        this.drawerLayout = findViewById(R.id.drawer)
+
+        setSupportActionBar(toolbar)
+
+        setupNavigation()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(findNavController(this, R.id.nav_host_fragment), drawerLayout)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+
+        // Update action bar to reflect navigation
+        setupActionBarWithNavController(navController, drawerLayout)
+
+        // Handle nav drawer item clicks
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        // Tie nav graph to items in nav drawer
+        setupWithNavController(navigationView, navController)
     }
 }
--- app/src/main/java/com/quaksire/weatherapp/SettingsFragment.kt
+++ app/src/main/java/com/quaksire/weatherapp/SettingsFragment.kt
@@ -1,11 +1,11 @@
 package com.quaksire.weatherapp
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsFragment : Fragment() {
 
@@ -14,6 +14,10 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        (activity as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(true)
         return inflater.inflate(R.layout.fragment_settings, container, false)
+
     }
 }
