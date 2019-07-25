7276c42ba16bd7ad0a53762505f3baec55ace96a
quaksire.group <quaksire.group@gmail.com>
2019-07-25 06:57:54 +0100

    Add location navigation

--- app/src/main/java/com/quaksire/weatherapp/CitiesFragment.kt
+++ app/src/main/java/com/quaksire/weatherapp/CitiesFragment.kt
@@ -3,10 +3,13 @@ package com.quaksire.weatherapp
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CitiesFragment : Fragment() {
 
     private lateinit var layoutView: View
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +17,10 @@ class CitiesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         layoutView = inflater.inflate(R.layout.fragment_cities, container, false)
+
+        layoutView.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_cities_to_add_city)
+        }
 
         return layoutView
     }
